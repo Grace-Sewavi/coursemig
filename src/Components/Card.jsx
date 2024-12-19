be9ed18d5d9d65b31873728d3e4f04c3c8bd390a
sew-53 <sewavigrace2005@gmail.com>
2024-12-19 07:21:03 -0800

    commit-1

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -42,7 +42,7 @@ export default function Card() {
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 1``,
+            slidesToShow: 1,
             slidesToScroll: 1,
             infinite: true,
             dots: true,
